867886336cb4bd42407dd15d641056fd15623ea0
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-08-26 19:51:17 +0200

    dependency injection

--- src/api/Codend.Api/Program.cs
+++ src/api/Codend.Api/Program.cs
@@ -1,3 +1,6 @@
+using Codend.Application;
+using Codend.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/core/Codend.Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Codend.Persistence;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Registers the necessary services with the DI framework.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configuration">The configuration.</param>
+    /// <returns>The same service collection.</returns>
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
