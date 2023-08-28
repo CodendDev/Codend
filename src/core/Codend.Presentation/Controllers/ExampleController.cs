c41b53d334f92c2be2b5a9a5ac3ac8199045fb4e
pzajaczkowski <demoon.pps@gmail.com>
2023-08-28 12:51:52 +0200

    Add api controller abstraction + test controller

---
+++ src/core/Codend.Presentation/Controllers/ExampleController.cs
@@ -0,0 +1,24 @@
+﻿using Codend.Presentation.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codend.Presentation.Controllers;
+
+public class ExampleController : ApiController
+{
+    public ExampleController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet("test-ok")]
+    public IActionResult TestOk()
+    {
+        return Ok();
+    }
+    
+    [HttpGet("test-bad-request")]
+    public IActionResult TestBadRequest()
+    {
+        return BadRequest();
+    }
+}
---
+++ src/core/Codend.Presentation/Infrastructure/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codend.Presentation.Infrastructure;
+
+[Route("api")]
+public class ApiController : ControllerBase
+{
+    protected ApiController(IMediator mediator) => Mediator = mediator;
+    
+    protected IMediator Mediator { get; }
+}
