0c57199d916379a127603426ef93ae450875900d
pzajaczkowski <demoon.pps@gmail.com>
2023-08-28 10:01:34 +0200

    Rename AggregateRoot to Aggregate

---
+++ src/core/Codend.Domain/Core/Primitives/Aggregate.cs
@@ -0,0 +1,19 @@
+﻿using Codend.Domain.Core.Events;
+
+namespace Codend.Domain.Core.Primitives;
+
+public abstract class Aggregate<TKey> : Entity<TKey>
+{
+    protected Aggregate(TKey id) : base(id)
+    {
+    }
+
+    private readonly List<DomainEvent> _domainEvents = new();
+
+    public IReadOnlyCollection<DomainEvent> DomainEvents => this._domainEvents;
+
+    protected void Raise(DomainEvent domainEvent)
+    {
+        this._domainEvents.Add(domainEvent);
+    }
+}
