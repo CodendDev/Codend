47be9e0011d2b3809b715d3e876ef1e3aadfff91
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-08-29 10:22:29 +0200

    Domain errors static class

---
+++ src/core/Codend.Domain/Core/Errors/DomainErrors.cs
@@ -0,0 +1,18 @@
+﻿using FluentResults;
+
+namespace Codend.Domain.Core.Errors;
+
+/// <summary>
+/// Contains the domain errors.
+/// </summary>
+public static class DomainErrors
+{
+    public abstract class DomainError : Error
+    {
+        protected DomainError(string errorCode, string message)
+        {
+            Metadata.Add("ErrorCode", errorCode);
+            Metadata.Add("Message", message);
+        }
+    }
+}
