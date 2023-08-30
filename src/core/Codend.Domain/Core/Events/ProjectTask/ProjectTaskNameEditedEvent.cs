e5e25c9d7bc29f37a32ab7fc1cd2d9f800bf550c
pzajaczkowski <demoon.pps@gmail.com>
2023-08-30 13:53:05 +0200

    Add ProjectTask domain events.

---
+++ src/core/Codend.Domain/Core/Events/ProjectTask/ProjectTaskNameEditedEvent.cs
@@ -0,0 +1,20 @@
+﻿using Codend.Domain.Core.Abstractions;
+using Codend.Domain.Entities;
+using Codend.Domain.ValueObjects;
+
+namespace Codend.Domain.Core.Events.ProjectTask;
+
+/// <summary>
+/// Domain event raised after ProjectTaskName has been changed
+/// </summary>
+public class ProjectTaskNameEditedEvent : IDomainEvent
+{
+    public ProjectTaskNameEditedEvent(ProjectTaskName name, ProjectTaskId projectTaskId)
+    {
+        Name = name;
+        ProjectTaskId = projectTaskId;
+    }
+
+    public ProjectTaskName Name { get; set; }
+    public ProjectTaskId ProjectTaskId { get; set; }
+}
