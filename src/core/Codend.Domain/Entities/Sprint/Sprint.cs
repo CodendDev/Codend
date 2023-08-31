645102213ef96fdca1ed185aecd5138963a51da2
pzajaczkowski <demoon.pps@gmail.com>
2023-08-31 16:50:41 +0200

    Add sprint domain methods and event.

--- src/core/Codend.Domain/Entities/Sprint/Sprint.cs
+++ src/core/Codend.Domain/Entities/Sprint/Sprint.cs
@@ -1,20 +1,46 @@
 using Codend.Domain.Core.Abstractions;
+using Codend.Domain.Core.Extensions;
 using Codend.Domain.Core.Primitives;
 using Codend.Domain.ValueObjects;
+using FluentResults;
 
 namespace Codend.Domain.Entities;
 
 public class Sprint : Entity<SprintId>, ISoftDeletableEntity
 {
-    private Sprint(SprintId id) : base(id)
+    private Sprint() : base(new SprintId(Guid.NewGuid()))
     {
     }
-    
+
     public SprintPeriod Period { get; private set; }
     public SprintGoal? Goal { get; private set; }
     public ProjectId ProjectId { get; private set; }
     public virtual List<ProjectTask> SprintProjectTasks { get; private set; }
-    
+
     public DateTime DeletedOnUtc { get; private set; }
     public bool Deleted { get; }
+
+    public static Result<Sprint> Create(ProjectId projectId, DateTime startDate, DateTime endDate, string? goal)
+    {
+        var sprint = new Sprint()
+        {
+            ProjectId = projectId
+        };
+
+        var periodResult = SprintPeriod.Create(startDate, endDate);
+        var goalResult = goal is null ? null : SprintGoal.Create(goal);
+
+        sprint.Period = periodResult.ValueOrDefault;
+        if (goalResult is null)
+        {
+            sprint.Goal = null;
+            goalResult = Result.Ok();
+        }
+        else sprint.Goal = goalResult.ValueOrDefault;
+
+        return Result.Ok(sprint)
+            .MergeReasons(
+                periodResult.ToResult(),
+                goalResult.ToResult());
+    }
 }
