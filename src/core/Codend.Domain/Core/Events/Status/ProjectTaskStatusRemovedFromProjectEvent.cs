93db254cfe13b53cba0b6737df9f58821e972bc0
pzajaczkowski <demoon.pps@gmail.com>
2023-09-01 12:57:20 +0200

    Add ProjectTaskStatus domain methods and events.

---
+++ src/core/Codend.Domain/Core/Events/Status/ProjectTaskStatusRemovedFromProjectEvent.cs
@@ -0,0 +1,19 @@
+﻿using Codend.Domain.Core.Abstractions;
+using Codend.Domain.Entities;
+
+namespace Codend.Domain.Core.Events;
+
+/// <summary>
+/// Domain event raised after removing status from project.
+/// </summary>
+public class ProjectTaskStatusRemovedFromProjectEvent : IDomainEvent
+{
+    public ProjectTaskStatusRemovedFromProjectEvent(ProjectTaskStatus projectTaskStatus, ProjectId projectId)
+    {
+        ProjectTaskStatus = projectTaskStatus;
+        ProjectId = projectId;
+    }
+
+    public ProjectTaskStatus ProjectTaskStatus { get; set; }
+    public ProjectId ProjectId { get; set; }
+}
--- src/core/Codend.Domain/Entities/ProjectTaskStatus/ProjectTaskStatus.cs
+++ src/core/Codend.Domain/Entities/ProjectTaskStatus/ProjectTaskStatus.cs
@@ -1,15 +1,49 @@
+using Codend.Domain.Core.Extensions;
 using Codend.Domain.Core.Primitives;
 using Codend.Domain.ValueObjects;
+using FluentResults;
 
 namespace Codend.Domain.Entities;
 
 public class ProjectTaskStatus : Entity<ProjectTaskStatusId>
 {
-    public ProjectTaskStatus(ProjectTaskStatusId id) : base(id)
+    private ProjectTaskStatus() : base(new ProjectTaskStatusId(Guid.NewGuid()))
     {
     }
 
     public ProjectTaskStatusName Name { get; private set; }
 
     public ProjectId ProjectId { get; private set; }
+
+    /// <summary>
+    /// Creates new projectTaskStatus.
+    /// </summary>
+    /// <param name="projectId">Id of project it belongs to.</param>
+    /// <param name="name">Name for the status.</param>
+    /// <returns>Ok result with newly created ProjectTaskStatus object or an error.</returns>
+    public static Result<ProjectTaskStatus> Create(ProjectId projectId, string name)
+    {
+        var status = new ProjectTaskStatus()
+        {
+            ProjectId = projectId
+        };
+
+        var nameResult = ProjectTaskStatusName.Create(name);
+        status.Name = nameResult.ValueOrDefault;
+
+        return Result.Ok(status).MergeReasons(nameResult.ToResult());
+    }
+
+    /// <summary>
+    /// Edits name of the ProjectTaskStatus.
+    /// </summary>
+    /// <param name="name">Name for the status.</param>
+    /// <returns>Ok result with ProjectTaskStatus object witch new name or an error.</returns>
+    public Result<ProjectTaskStatus> EditName(string name)
+    {
+        var nameResult = ProjectTaskStatusName.Create(name);
+        this.Name = nameResult.ValueOrDefault;
+
+        return Result.Ok(this).MergeReasons(nameResult.ToResult());
+    }
 }
