5103d8c85f7cdf8ed6fdb1cde2b8b8577a097015
pzajaczkowski <demoon.pps@gmail.com>
2023-09-04 17:16:26 +0200

    Create IPageableQuery and ISortableQuery with IQueryable extension methods.

---
+++ src/core/Codend.Application/Core/Abstractions/Querying/IPageableQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace Codend.Application.Core.Abstractions.Querying;
+
+/// <summary>
+/// Paging criteria.
+/// </summary>
+public interface IPageableQuery
+{
+    /// <summary>
+    /// Page size.
+    /// </summary>
+    int PageSize { get; set; }
+
+    /// <summary>
+    /// Page index.
+    /// </summary>
+    int PageIndex { get; set; }
+}
