a8079971703a3e4ed9eccec3209fadb259ec97a1
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-05 13:32:55 +0200

    Creating default project tasks statuses with project creation

--- src/core/Codend.Application/Projects/Commands/CreateProject/CreateProjectCommand.cs
+++ src/core/Codend.Application/Projects/Commands/CreateProject/CreateProjectCommand.cs
@@ -1,6 +1,7 @@
 using Codend.Application.Core.Abstractions.Authentication;
 using Codend.Application.Core.Abstractions.Data;
 using Codend.Application.Core.Abstractions.Messaging.Commands;
+using Codend.Domain.Core.Enums;
 using Codend.Domain.Entities;
 using Codend.Domain.Repositories;
 using FluentResults;
@@ -15,31 +16,47 @@ public sealed record CreateProjectCommand(
 public class CreateProjectCommandHandler : ICommandHandler<CreateProjectCommand, Guid>
 {
     private readonly IProjectRepository _projectRepository;
+    private readonly IProjectTaskStatusRepository _statusRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IUserIdentityProvider _identityProvider;
 
     public CreateProjectCommandHandler(
         IProjectRepository projectRepository,
         IUnitOfWork unitOfWork,
-        IUserIdentityProvider identityProvider)
+        IUserIdentityProvider identityProvider,
+        IProjectTaskStatusRepository statusRepository)
     {
         _projectRepository = projectRepository;
         _unitOfWork = unitOfWork;
         _identityProvider = identityProvider;
+        _statusRepository = statusRepository;
     }
 
     public async Task<Result<Guid>> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
     {
-        var result = Project.Create(_identityProvider.UserId, request.Name, request.Description);
+        var resultProject = Project.Create(_identityProvider.UserId, request.Name, request.Description);
+        if (resultProject.IsFailed)
+        {
+            return resultProject.ToResult();
+        }
+
+        var project = resultProject.Value;
+
+        var resultStatuses = DefaultTaskStatus.List
+            .Select(status => ProjectTaskStatus.Create(project.Id, status.Name))
+            .ToList();
+        var result = resultStatuses.Merge();
         if (result.IsFailed)
         {
             return result.ToResult();
         }
 
-        var project = result.Value;
+        var statuses = resultStatuses.Select(r => r.Value);
+
+        await _statusRepository.AddRangeAsync(statuses);
         _projectRepository.Add(project);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return result.Value.Id.Value;
+        return project.Id.Value;
     }
 }
