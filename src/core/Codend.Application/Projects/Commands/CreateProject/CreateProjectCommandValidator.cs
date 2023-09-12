b52c73ec92c36dac8c839ade7d530c91754d6f40
pzajaczkowski <demoon.pps@gmail.com>
2023-09-12 15:09:16 +0200

    Add common validation errors and CreateProjectCommand validator as an example

---
+++ src/core/Codend.Application/Projects/Commands/CreateProject/CreateProjectCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+using Codend.Application.Core.Errors;
+using Codend.Application.Extensions;
+using Codend.Domain.ValueObjects;
+using FluentValidation;
+
+namespace Codend.Application.Projects.Commands.CreateProject;
+
+/// <summary>
+/// Validates create project command.
+/// </summary>
+public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CreateProjectCommandValidator"/> class.
+    /// </summary>
+    public CreateProjectCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithError(new ValidationErrors.Common.StringPropertyNullOrEmpty(nameof(CreateProjectCommand.Name)));
+        RuleFor(x => x.Name)
+            .MaximumLength(ProjectName.MaxLength)
+            .WithError(new ValidationErrors.Common.StringPropertyTooLong(nameof(CreateProjectCommand.Name),
+                ProjectName.MaxLength));
+        RuleFor(x => x.Description)
+            .MaximumLength(ProjectDescription.MaxLength)
+            .When(x => !string.IsNullOrEmpty(x.Description))
+            .WithError(new ValidationErrors.Common.StringPropertyTooLong(nameof(CreateProjectCommand.Description),
+                ProjectDescription.MaxLength));
+    }
+}
