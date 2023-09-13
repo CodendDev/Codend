79dc5c1d5f49c52f8daca00ebb4291258ed28737
pzajaczkowski <demoon.pps@gmail.com>
2023-09-13 12:41:38 +0200

    Register ProjectMemberRepository.cs and add new GetByProjectAndMemberId method.

--- src/core/persistence/Codend.Persistence/Repositories/ProjectMemberRepository.cs
+++ src/core/persistence/Codend.Persistence/Repositories/ProjectMemberRepository.cs
@@ -1,5 +1,6 @@
 using Codend.Domain.Entities;
 using Codend.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Codend.Persistence.Repositories;
 
@@ -7,5 +8,12 @@ public class ProjectMemberRepository : GenericRepository<ProjectMemberId, Guid, 
 {
     public ProjectMemberRepository(CodendApplicationDbContext context) : base(context)
     {
+    }
+    
+    public Task<ProjectMember?> GetByProjectAndMemberId(ProjectId projectId, UserId memberId, CancellationToken cancellationToken)
+    {
+        return Context.Set<ProjectMember>().FirstOrDefaultAsync(projectMember =>
+            projectMember.ProjectId.Value.Equals(projectId.Value) &&
+            projectMember.MemberId.Value.Equals(memberId.Value), cancellationToken: cancellationToken);
     }
 }
