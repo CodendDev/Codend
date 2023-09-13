594379b7c350ce0dd4cd28349817aedcf9c399b0
pzajaczkowski <demoon.pps@gmail.com>
2023-09-13 13:47:14 +0200

    Change validator to allow only owner to delete Project

--- src/core/Codend.Application/Projects/Commands/DeleteProject/DeleteProjectCommandValidator.cs
+++ src/core/Codend.Application/Projects/Commands/DeleteProject/DeleteProjectCommandValidator.cs
@@ -12,29 +12,30 @@ namespace Codend.Application.Projects.Commands.DeleteProject;
 /// </summary>
 public class DeleteProjectCommandValidator : AbstractValidator<DeleteProjectCommand>
 {
-    private readonly IProjectMemberRepository _projectMemberRepository;
+    private readonly IProjectRepository _projectRepository;
     private readonly IUserIdentityProvider _identityProvider;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="DeleteProjectCommandValidator"/> class.
     /// </summary>
-    public DeleteProjectCommandValidator(IProjectMemberRepository projectMemberRepository,
+    public DeleteProjectCommandValidator(IProjectRepository projectRepository,
         IUserIdentityProvider identityProvider)
     {
-        _projectMemberRepository = projectMemberRepository;
+        _projectRepository = projectRepository;
         _identityProvider = identityProvider;
 
         RuleFor(x => x.ProjectId)
-            .MustAsync(ProjectExistsAndUserIsMember)
-            .WithError(new ValidationErrors.Project.NotFoundOrUserUnauthorized());
+            .MustAsync(ProjectExistsAndUserIsOwner)
+            .WithError(new ValidationErrors.Project.NotFoundOrUserUnauthorized())
+            .WithSeverity(Severity.Warning);
     }
 
-    private async Task<bool> ProjectExistsAndUserIsMember(Guid projectId, CancellationToken cancellationToken)
+    private async Task<bool> ProjectExistsAndUserIsOwner(Guid projectId, CancellationToken cancellationToken)
     {
         var userId = _identityProvider.UserId;
-        var projectMember =
-            await _projectMemberRepository.GetByProjectAndMemberId(new ProjectId(projectId), userId, cancellationToken);
+        var project =
+            await _projectRepository.GetByIdAsync(new ProjectId(projectId));
 
-        return projectMember is not null;
+        return project is not null && project.OwnerId.Equals(userId);
     }
 }
