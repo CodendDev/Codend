58013f1961af09e89194be73e3ee7350f9c8ec6a
pzajaczkowski <demoon.pps@gmail.com>
2023-09-13 11:38:00 +0200

    Add ProjectMember configuration + migration

---
+++ src/core/persistence/Codend.Persistence.Postgres/Migrations/20230913093235_ProjectMember-Relation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codend.Persistence.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectMemberRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProjectMember",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsFavourite = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp(0) with time zone", precision: 0, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectMember", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectMember_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMember_ProjectId",
+                table: "ProjectMember",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectMember");
+        }
+    }
+}
