b6cb6c1701f46f68e2f29cb11cda04c54d9db0e3
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-13 12:28:03 +0200

    Create task request documentation

--- src/core/Codend.Contracts/Requests/ProjectTasks/Create/ICreateBaseProjectTaskRequest.cs
+++ src/core/Codend.Contracts/Requests/ProjectTasks/Create/ICreateBaseProjectTaskRequest.cs
@@ -1,15 +1,53 @@
 ﻿namespace Codend.Contracts.Requests.ProjectTasks.Create;
 
+/// <summary>
+/// Request used for creating BaseProjectTask
+/// </summary>
 public interface ICreateBaseProjectTaskRequest
 {
+    /// <summary>
+    /// Name of the task.
+    /// </summary>
     string Name { get; }
+
+    /// <summary>
+    /// Priority of the task.
+    /// </summary>
     string Priority { get; }
+
+    /// <summary>
+    /// StatusId of the task. Status and task must belong to the same project.
+    /// </summary>
     Guid StatusId { get; }
+
+    /// <summary>
+    /// ProjectId to which task will be assigned.
+    /// </summary>
     Guid ProjectId { get; }
+
+    /// <summary>
+    /// Description of the task.
+    /// </summary>
     string? Description { get; }
+
+    /// <summary>
+    /// Estimated time of the task completion.
+    /// </summary>
     EstimatedTimeRequest? EstimatedTime { get; }
+
+    /// <summary>
+    /// Due date by which task has to be done.
+    /// </summary>
     DateTime? DueDate { get; }
+
+    /// <summary>
+    /// Story points of the task.
+    /// </summary>
     uint? StoryPoints { get; }
+
+    /// <summary>
+    /// UserId of the Assignee.
+    /// </summary>
     Guid? AssigneeId { get; }
 
     /// <summary>
