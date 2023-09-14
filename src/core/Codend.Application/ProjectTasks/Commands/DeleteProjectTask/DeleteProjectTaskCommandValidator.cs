2f3fe0aa723869bb8b6433e31ffe0bed9f3f1ada
pzajaczkowski <demoon.pps@gmail.com>
2023-09-14 18:33:33 +0200

    Add DeleteProjectTaskCommandValidator.cs

---
+++ src/core/Codend.Application/ProjectTasks/Commands/DeleteProjectTask/DeleteProjectTaskCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using Codend.Application.Core.Errors;
+using Codend.Application.Extensions;
+using FluentValidation;
+
+namespace Codend.Application.ProjectTasks.Commands.DeleteProjectTask;
+
+/// <summary>
+/// Validator for <see cref="DeleteProjectTaskCommand"/>
+/// </summary>
+public class DeleteProjectTaskCommandValidator : AbstractValidator<DeleteProjectTaskCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeleteProjectTaskCommandValidator"/> class.
+    /// </summary>
+    public DeleteProjectTaskCommandValidator()
+    {
+        RuleFor(x => x.ProjectTaskId)
+            .NotEmpty()
+            .WithError(new ValidationErrors.Common.PropertyNullOrEmpty(nameof(DeleteProjectTaskCommand.ProjectTaskId)));
+    }
+}
