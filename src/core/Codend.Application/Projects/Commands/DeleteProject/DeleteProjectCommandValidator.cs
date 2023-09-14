fb078ff6ac0a76610145371ff178713ff3fa69e8
pzajaczkowski <demoon.pps@gmail.com>
2023-09-14 19:19:06 +0200

    Refactor DeleteProjectCommand.cs + add comments

--- src/core/Codend.Application/Projects/Commands/DeleteProject/DeleteProjectCommandValidator.cs
+++ src/core/Codend.Application/Projects/Commands/DeleteProject/DeleteProjectCommandValidator.cs
@@ -1,8 +1,5 @@
-﻿using Codend.Application.Core.Abstractions.Authentication;
-using Codend.Application.Core.Errors;
+﻿using Codend.Application.Core.Errors;
 using Codend.Application.Extensions;
-using Codend.Domain.Entities;
-using Codend.Domain.Repositories;
 using FluentValidation;
 
 namespace Codend.Application.Projects.Commands.DeleteProject;
@@ -12,30 +9,13 @@ namespace Codend.Application.Projects.Commands.DeleteProject;
 /// </summary>
 public class DeleteProjectCommandValidator : AbstractValidator<DeleteProjectCommand>
 {
-    private readonly IProjectRepository _projectRepository;
-    private readonly IUserIdentityProvider _identityProvider;
-
     /// <summary>
     /// Initializes a new instance of the <see cref="DeleteProjectCommandValidator"/> class.
     /// </summary>
-    public DeleteProjectCommandValidator(IProjectRepository projectRepository,
-        IUserIdentityProvider identityProvider)
+    public DeleteProjectCommandValidator()
     {
-        _projectRepository = projectRepository;
-        _identityProvider = identityProvider;
-
         RuleFor(x => x.ProjectId)
-            .MustAsync(ProjectExistsAndUserIsOwner)
-            .WithError(new ValidationErrors.Project.NotFoundOrUserUnauthorized())
-            .WithSeverity(Severity.Warning);
-    }
-
-    private async Task<bool> ProjectExistsAndUserIsOwner(Guid projectId, CancellationToken cancellationToken)
-    {
-        var userId = _identityProvider.UserId;
-        var project =
-            await _projectRepository.GetByIdAsync(new ProjectId(projectId));
-
-        return project is not null && project.OwnerId.Equals(userId);
+            .NotEmpty()
+            .WithError(new ValidationErrors.Common.PropertyNullOrEmpty(nameof(DeleteProjectCommand.ProjectId)));
     }
 }
