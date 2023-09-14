fafd79d46fb8fc64818886773a7a4f1b92f73903
pzajaczkowski <demoon.pps@gmail.com>
2023-09-14 18:30:49 +0200

    Add bugfix and base UpdateProjectTaskCommand validators.

---
+++ src/core/Codend.Application/ProjectTasks/Commands/UpdateProjectTask/UpdateBaseProjectTaskCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using Codend.Application.ProjectTasks.Commands.UpdateProjectTask.Abstractions;
+
+namespace Codend.Application.ProjectTasks.Commands.UpdateProjectTask;
+
+/// <summary>
+/// Validator for BaseProjectTask class.
+/// </summary>
+public class UpdateBaseProjectTaskCommandValidator
+    : UpdateProjectTaskCommandAbstractValidator<UpdateBaseProjectTaskCommand>
+{
+}
---
+++ src/core/Codend.Application/ProjectTasks/Commands/UpdateProjectTask/UpdateBugifxProjectTaskCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using Codend.Application.ProjectTasks.Commands.UpdateProjectTask.Abstractions;
+
+namespace Codend.Application.ProjectTasks.Commands.UpdateProjectTask;
+
+/// <summary>
+/// Validator for BugfixProjectTask class.
+/// </summary>
+public class UpdateBugfixProjectTaskCommandValidator
+    : UpdateProjectTaskCommandAbstractValidator<UpdateBugfixProjectTaskCommand>
+{
+}
