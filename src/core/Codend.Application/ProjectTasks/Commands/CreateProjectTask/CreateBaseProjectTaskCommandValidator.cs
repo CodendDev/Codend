e59f395d4e58767bb868caa98c566f14417971bc
pzajaczkowski <demoon.pps@gmail.com>
2023-09-14 15:35:51 +0200

    Add validator for CreateBaseProjectTask command.

---
+++ src/core/Codend.Application/ProjectTasks/Commands/CreateProjectTask/CreateBaseProjectTaskCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Codend.Application.Core.Abstractions.Authentication;
+using Codend.Application.ProjectTasks.Commands.CreateProjectTask.Abstractions;
+using Codend.Domain.Entities;
+using Codend.Domain.Repositories;
+
+namespace Codend.Application.ProjectTasks.Commands.CreateProjectTask;
+
+/// <summary>
+/// Validator for <see cref="CreateBaseProjectTaskCommand"/>
+/// </summary>
+public class CreateBaseProjectTaskCommandValidator : CreateProjectTaskCommandAbstractValidator<
+    CreateBaseProjectTaskCommand, BaseProjectTaskCreateProperties>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CreateBaseProjectTaskCommandValidator"/> class.
+    /// </summary>
+    public CreateBaseProjectTaskCommandValidator(IUserIdentityProvider identityProvider,
+        IProjectMemberRepository projectMemberRepository, IStoryRepository storyRepository,
+        IProjectTaskStatusRepository projectTaskStatusRepository) : base(identityProvider, projectMemberRepository,
+        storyRepository, projectTaskStatusRepository)
+    {
+    }
+}
