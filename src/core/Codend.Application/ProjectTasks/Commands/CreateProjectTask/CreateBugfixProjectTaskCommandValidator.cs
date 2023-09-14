1f23da0ebe259a968ed3f8472bfb4d3913da6c04
pzajaczkowski <demoon.pps@gmail.com>
2023-09-14 15:35:58 +0200

    Add validator for CreateBugfixProjectTask command.

---
+++ src/core/Codend.Application/ProjectTasks/Commands/CreateProjectTask/CreateBugfixProjectTaskCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Codend.Application.Core.Abstractions.Authentication;
+using Codend.Application.ProjectTasks.Commands.CreateProjectTask.Abstractions;
+using Codend.Domain.Entities.ProjectTask.Bugfix;
+using Codend.Domain.Repositories;
+
+namespace Codend.Application.ProjectTasks.Commands.CreateProjectTask;
+
+/// <summary>
+/// Validator for <see cref="CreateBugfixProjectTaskCommand"/>
+/// </summary>
+public class CreateBugfixProjectTaskCommandValidator : CreateProjectTaskCommandAbstractValidator<
+    CreateBugfixProjectTaskCommand, BugfixProjectTaskCreateProperties>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CreateBugfixProjectTaskCommandValidator"/> class.
+    /// </summary>
+    public CreateBugfixProjectTaskCommandValidator(IUserIdentityProvider identityProvider,
+        IProjectMemberRepository projectMemberRepository, IStoryRepository storyRepository,
+        IProjectTaskStatusRepository projectTaskStatusRepository) : base(identityProvider, projectMemberRepository,
+        storyRepository, projectTaskStatusRepository)
+    {
+    }
+}
