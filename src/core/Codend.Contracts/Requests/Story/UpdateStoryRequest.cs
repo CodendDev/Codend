7ea9a2a30669d8f78d62e8467677d08ccfb70be0
pzajaczkowski <demoon.pps@gmail.com>
2023-09-15 14:14:10 +0200

    Refactor StoryController.cs

---
+++ src/core/Codend.Contracts/Requests/Story/UpdateStoryRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Codend.Contracts.Requests.Story;
+
+/// <summary>
+/// Represents update story request.
+/// </summary>
+/// <param name="Name">Name of the story.</param>
+/// <param name="Description">Description of the story.</param>
+public record UpdateStoryRequest(
+    string Name,
+    string Description);
