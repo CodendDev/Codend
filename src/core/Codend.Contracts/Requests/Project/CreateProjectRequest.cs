26b303c4721fe79716e98a405c243fc1a9c3b42a
pzajaczkowski <demoon.pps@gmail.com>
2023-09-15 12:27:01 +0200

    Add create, update project requests.

---
+++ src/core/Codend.Contracts/Requests/Project/CreateProjectRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Codend.Contracts.Requests.Project;
+
+/// <summary>
+/// Represents the create project request.
+/// </summary>
+/// <param name="Name">Name of the project.</param>
+/// <param name="Description">Project description.</param>
+public sealed record CreateProjectRequest(
+    string Name,
+    string? Description);
