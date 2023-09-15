b47bec44d3211463c424a7dca4f469604d3fe576
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-15 20:28:10 +0200

    EntityId abstract class

---
+++ src/core/Codend.Domain/Core/Primitives/EntityIdExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Codend.Domain.Core.Primitives;
+
+public static class EntityIdExtensions
+{
+    public static TEntityId? ToKeyGuid<TEntityId>(Guid? value)
+        where TEntityId : EntityId<Guid>, new()
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        return new TEntityId { Value = value.Value };
+    }
+}
