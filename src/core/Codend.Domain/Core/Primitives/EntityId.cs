c112200f03005b34ad8bf4c8228f36381cba9d58
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-15 20:28:10 +0200

    EntityId abstract class

---
+++ src/core/Codend.Domain/Core/Primitives/EntityId.cs
@@ -0,0 +1,23 @@
+﻿using Codend.Domain.Core.Abstractions;
+
+namespace Codend.Domain.Core.Primitives;
+
+public abstract class EntityId<TKey> : IEntityId<TKey>
+{
+    protected EntityId()
+    {
+    }
+
+    protected EntityId(TKey value)
+    {
+        _value = value;
+    }
+
+    private readonly TKey? _value;
+
+    public TKey Value
+    {
+        get => _value ?? throw new NullReferenceException("Id cannot be null!");
+        init => _value = value;
+    }
+}
