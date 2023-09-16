5d215c05b99f3fd97d471f451618eb3bcb32199a
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-16 19:29:17 +0200

    Delete Epic request action

--- src/core/Codend.Presentation/Controllers/EpicController.cs
+++ src/core/Codend.Presentation/Controllers/EpicController.cs
@@ -1,4 +1,6 @@
 using Codend.Application.Epics.Commands.CreateEpic;
+using Codend.Application.Epics.Commands.DeleteEpic;
+using Codend.Application.Stories.Commands.CreateStory;
 using Codend.Contracts;
 using Codend.Contracts.Requests.Epic;
 using Codend.Domain.Entities;
@@ -50,5 +52,29 @@ public class EpicController : ApiController
         }
 
         return BadRequest(response.MapToApiErrorsResponse());
+    }
+
+    /// <summary>
+    /// Deletes epic with given <paramref name="epicId"/>.
+    /// </summary>
+    /// <param name="epicId">Id of the epic which will be deleted.</param>
+    /// <returns>
+    /// HTTP response with status code:
+    /// - 204 on success
+    /// - 404 on failure
+    /// </returns>
+    [HttpDelete("{epicId:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete([FromRoute] Guid epicId)
+    {
+        var command = new DeleteEpicCommand(epicId);
+        var response = await Mediator.Send(command);
+        if (response.IsSuccess)
+        {
+            return NoContent();
+        }
+
+        return NotFound();
     }
 }
