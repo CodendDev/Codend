dd80a08bab48914938079e0d287627eb6e9444b9
pzajaczkowski <demoon.pps@gmail.com>
2023-09-18 13:06:13 +0200

    Add authentication validation errors.

--- src/core/Codend.Application/Core/Errors/ValidationErrors.cs
+++ src/core/Codend.Application/Core/Errors/ValidationErrors.cs
@@ -1,4 +1,5 @@
-﻿using Codend.Domain.Core.Enums;
+﻿using Codend.Application.Core.Abstractions.Authentication;
+using Codend.Domain.Core.Enums;
 using Codend.Domain.Core.Errors;
 
 namespace Codend.Application.Core.Errors;
@@ -28,7 +29,7 @@ public static partial class ValidationErrors
         public class PropertyNullOrEmpty : ValidationError
         {
             /// <inheritdoc />
-            public PropertyNullOrEmpty(string stringName) : base("Common.StringPropertyNullOrEmpty",
+            public PropertyNullOrEmpty(string stringName) : base("Validation.Common.StringPropertyNullOrEmpty",
                 $"Field {stringName} cannot be null or empty.")
             {
             }
@@ -38,7 +39,8 @@ public static partial class ValidationErrors
         public class StringPropertyTooLong : ValidationError
         {
             /// <inheritdoc />
-            public StringPropertyTooLong(string stringName, int maxLength) : base("Common.StringPropertyTooLong",
+            public StringPropertyTooLong(string stringName, int maxLength) : base(
+                "Validation.Common.StringPropertyTooLong",
                 $"Field {stringName} is longer than maximum allowed length {maxLength}.")
             {
             }
@@ -48,7 +50,7 @@ public static partial class ValidationErrors
         public class DateIsInThePast : ValidationError
         {
             /// <inheritdoc />
-            public DateIsInThePast(string fieldName) : base("Common.DateIsInThePast",
+            public DateIsInThePast(string fieldName) : base("Validation.Common.DateIsInThePast",
                 $"Given date field {fieldName} is in the past.")
             {
             }
@@ -64,8 +66,100 @@ public static partial class ValidationErrors
         public class PriorityNotDefined : ValidationError
         {
             /// <inheritdoc />
-            public PriorityNotDefined() : base("ProjectTask.PriorityNotDefined",
-                $"Given priority is not defined. Valid priorities: '{string.Join(", ",ProjectTaskPriority.DefaultList())}'.")
+            public PriorityNotDefined() : base("Validation.ProjectTask.PriorityNotDefined",
+                $"Given priority is not defined. Valid priorities: '{string.Join(", ", ProjectTaskPriority.DefaultList())}'.")
+            {
+            }
+        }
+    }
+
+    /// <summary>
+    /// Email address validation error static class.
+    /// </summary>
+    public static class EmailAddress
+    {
+        /// <inheritdoc />
+        public class NotValid : ValidationError
+        {
+            /// <inheritdoc />
+            public NotValid() : base("Validation.EmailAddress.NotValid",
+                "Provided email is not valid email address.")
+            {
+            }
+        }
+
+        /// <inheritdoc />
+        public class TooLong : ValidationError
+        {
+            /// <inheritdoc />
+            public TooLong() : base("Validation.EmailAddress.TooLong",
+                $"Provided email is longer than allowed {IAuthService.MaxEmailLength}.")
+            {
+            }
+        }
+    }
+
+    /// <summary>
+    /// Password validation error static class.
+    /// </summary>
+    public static class Password
+    {
+        /// <inheritdoc />
+        public class TooLong : ValidationError
+        {
+            /// <inheritdoc />
+            public TooLong() : base("Validation.Password.TooLong",
+                $"Provided password is longer than allowed {IAuthService.MaxPasswordLength}.")
+            {
+            }
+        }
+
+        /// <inheritdoc />
+        public class TooShort : ValidationError
+        {
+            /// <inheritdoc />
+            public TooShort() : base("Validation.Password.TooShort",
+                $"Provided password is shorter than allowed {IAuthService.MinPasswordLength}.")
+            {
+            }
+        }
+        
+        /// <inheritdoc />
+        public class NotContainLowercaseLetter : ValidationError
+        {
+            /// <inheritdoc />
+            public NotContainLowercaseLetter() : base("Validation.Password.NotContainLowercaseLetter",
+                "Password must contain at least 1 lowercase letter.")
+            {
+            }
+        }
+        
+        /// <inheritdoc />
+        public class NotContainUppercaseLetter : ValidationError
+        {
+            /// <inheritdoc />
+            public NotContainUppercaseLetter() : base("Validation.Password.NotContainUppercaseLetter",
+                "Password must contain at least 1 uppercase letter.")
+            {
+            }
+        }
+        
+        /// <inheritdoc />
+        public class NotContainDigit : ValidationError
+        {
+            /// <inheritdoc />
+            public NotContainDigit() : base("Validation.Password.NotContainDigit",
+                "Password must contain at least 1 digit.")
+            {
+            }
+        }
+        
+        /// <inheritdoc />
+        public class NotContainCustomChar : ValidationError
+        {
+            /// <inheritdoc />
+            public NotContainCustomChar() : base("Validation.Password.NotContainCustomChar",
+                "Password must contain at least 1 of this custom chars: @$#!%*?&.")
             {
             }
         }
