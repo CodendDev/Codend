e32a3a99bf2856c267eb4c18a7f37b794766a394
pzajaczkowski <demoon.pps@gmail.com>
2023-09-19 11:58:44 +0200

    Remove misleading comments from ProjectController.cs

--- src/core/Codend.Presentation/Controllers/ProjectController.cs
+++ src/core/Codend.Presentation/Controllers/ProjectController.cs
@@ -103,7 +103,9 @@ public class ProjectController : ApiController
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(typeof(ApiErrorsResponse), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> Update([FromRoute] Guid projectId, [FromBody] UpdateProjectRequest request)
+    public async Task<IActionResult> Update(
+        [FromRoute] Guid projectId,
+        [FromBody] UpdateProjectRequest request)
     {
         var command = new UpdateProjectCommand(projectId, request.Name, request.Description);
         var response = await Mediator.Send(command);
@@ -130,7 +132,7 @@ public class ProjectController : ApiController
     [HttpGet("{projectId:guid}")]
     [ProducesResponseType(typeof(ProjectResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> Get(Guid projectId)
+    public async Task<IActionResult> Get([FromRoute] Guid projectId)
     {
         var query = new GetProjectByIdQuery(projectId);
         var response = await Mediator.Send(query);
@@ -147,14 +149,6 @@ public class ProjectController : ApiController
     /// </summary>
     /// <param name="projectId">The id of the project to which user will be added as member.</param>
     /// <param name="userId">The id of the user which will be added as member.</param>
-    /// <remarks>
-    /// Sample request:
-    ///
-    ///     {
-    ///         "projectId": "1f0c1930-50f4-4f17-8470-211b3a5cc873",
-    ///         "userId": "e405f337-4da0-4cce-818b-9231642c93fe"
-    ///     }
-    /// </remarks>
     /// <returns>
     /// HTTP response with status code:
     /// 204 - on success
@@ -165,7 +159,9 @@ public class ProjectController : ApiController
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> AddMember(Guid projectId, Guid userId)
+    public async Task<IActionResult> AddMember(
+        [FromRoute] Guid projectId,
+        [FromRoute] Guid userId)
     {
         var command = new AddMemberCommand(projectId.GuidConversion<ProjectId>(), userId.GuidConversion<UserId>());
         var response = await Mediator.Send(command);
@@ -187,14 +183,6 @@ public class ProjectController : ApiController
     /// </summary>
     /// <param name="projectId">The id of the project from which user will be removed.</param>
     /// <param name="userId">The id of the user which will be removed.</param>
-    /// <remarks>
-    /// Sample request:
-    ///
-    ///     {
-    ///         "projectId": "1f0c1930-50f4-4f17-8470-211b3a5cc873",
-    ///         "userId": "e405f337-4da0-4cce-818b-9231642c93fe"
-    ///     }
-    /// </remarks>
     /// <returns>
     /// HTTP response with status code:
     /// 204 - on success
@@ -203,7 +191,9 @@ public class ProjectController : ApiController
     [HttpDelete("{projectId:guid}/member/{userId:guid}")]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> RemoveMember(Guid projectId, Guid userId)
+    public async Task<IActionResult> RemoveMember(
+        [FromRoute] Guid projectId,
+        [FromRoute] Guid userId)
     {
         var command = new RemoveMemberCommand(projectId.GuidConversion<ProjectId>(), userId.GuidConversion<UserId>());
         var response = await Mediator.Send(command);
