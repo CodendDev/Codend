74f64c8de1659815e48e0512a7e7d78a3c96faa2
pzajaczkowski <demoon.pps@gmail.com>
2023-09-19 11:52:34 +0200

    Refactor epic controller routes to include projectId, and replace projectId in requests with one from route.

--- src/core/Codend.Contracts/Requests/Epic/CreateEpicRequest.cs
+++ src/core/Codend.Contracts/Requests/Epic/CreateEpicRequest.cs
@@ -5,10 +5,8 @@ namespace Codend.Contracts.Requests.Epic;
 /// </summary>
 /// <param name="Name">Name of the epic.</param>
 /// <param name="Description">Description of the epic.</param>
-/// <param name="ProjectId">Id of the project epic will be created for.</param>
 public record CreateEpicRequest
 (
     string Name,
-    string Description,
-    Guid ProjectId
+    string Description
 );
