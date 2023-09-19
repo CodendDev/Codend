064a6162cf8eca8e433830d05ec1bb20ad8550dd
pzajaczkowski <demoon.pps@gmail.com>
2023-09-19 15:24:14 +0200

    Register authorization handler.

---
+++ src/core/Codend.Presentation/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Codend.Presentation.Infrastructure.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Codend.Presentation;
+
+/// <summary>
+/// Dependency injection class.
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Registers the necessary services with the DI framework.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The same service collection.</returns>
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthorizationHandler, ProjectOperationsAuthorizationHandler>();
+        
+        return services;
+    }
+}
