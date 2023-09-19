0e91e97076d05474a62f820a3fd4443e94c0b97f
pzajaczkowski <demoon.pps@gmail.com>
2023-09-19 14:05:50 +0200

    Add project operations authorization requirements.

---
+++ src/core/Codend.Infrastructure/Authorization/ProjectOperations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Codend.Infrastructure.Authorization;
+
+/// <summary>
+/// This class defines authorization requirements for various operations on the project itself and
+/// all its dependent entities.
+/// </summary>
+public static class ProjectOperations
+{
+    /// <summary>
+    /// Represents the "edit" operation authorization requirement,
+    /// which allows to manage project properties and entities.
+    /// </summary>
+    public static readonly OperationAuthorizationRequirement Edit = new() { Name = nameof(Edit) };
+    
+    /// <summary>
+    /// Represents the "delete" operation authorization requirement,
+    /// which allows to delete project.
+    /// </summary>
+    public static readonly OperationAuthorizationRequirement Delete = new() { Name = nameof(Delete) };
+}
