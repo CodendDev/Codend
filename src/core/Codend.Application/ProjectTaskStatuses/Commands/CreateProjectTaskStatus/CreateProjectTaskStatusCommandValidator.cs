bcfda1b64a94b5cfd0f5e74b22ea562aa5a3ff50
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-25 10:11:55 +0200

    Create status command validator

---
+++ src/core/Codend.Application/ProjectTaskStatuses/Commands/CreateProjectTaskStatus/CreateProjectTaskStatusCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using Codend.Application.Extensions;
+using Codend.Domain.ValueObjects;
+using FluentValidation;
+using static Codend.Application.Core.Errors.ValidationErrors.Common;
+
+namespace Codend.Application.ProjectTaskStatuses.Commands.CreateProjectTaskStatus;
+
+/// <summary>
+/// <see cref="CreateProjectTaskStatusCommand"/> validator.
+/// </summary>
+public class CreateProjectTaskStatusCommandValidator : AbstractValidator<CreateProjectTaskStatusCommand>
+{
+    /// <summary>
+    /// Initializes validation rules for <see cref="CreateProjectTaskStatusCommand"/>.
+    /// </summary>
+    public CreateProjectTaskStatusCommandValidator()
+    {
+        RuleFor(x => x.ProjectId)
+            .NotEmpty()
+            .WithError(new PropertyNullOrEmpty(nameof(CreateProjectTaskStatusCommand.ProjectId)));
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithError(new PropertyNullOrEmpty(nameof(CreateProjectTaskStatusCommand.Name)))
+            .MaximumLength(ProjectTaskStatusName.MaxLength)
+            .WithError(new StringPropertyTooLong(nameof(CreateProjectTaskStatusCommand.Name), ProjectTaskStatusName.MaxLength));
+    }
+}
