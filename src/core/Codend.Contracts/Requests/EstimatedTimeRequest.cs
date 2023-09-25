aa20276dd231ba665a08df857f0a78be10dc1361
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-09-25 12:36:34 +0200

    Contracts requests documentation

--- src/core/Codend.Contracts/Requests/EstimatedTimeRequest.cs
+++ src/core/Codend.Contracts/Requests/EstimatedTimeRequest.cs
@@ -1,5 +1,8 @@
 namespace Codend.Contracts.Requests;
 
+/// <summary>
+/// Represents EstimatedTime request. 
+/// </summary>
 public record EstimatedTimeRequest
 (
     uint Minutes,
@@ -7,8 +10,17 @@ public record EstimatedTimeRequest
     uint Days
 );
 
+/// <summary>
+/// <see cref="EstimatedTimeRequest"/> extensions.
+/// </summary>
 public static class EstimatedTimeRequestExtensions
 {
+    /// <summary>
+    /// Converts <see cref="EstimatedTimeRequest"/> object to <see cref="TimeSpan"/> object.
+    /// If <see cref="EstimatedTimeRequest"/> is null returns null.
+    /// </summary>
+    /// <param name="request">Request to be converted.</param>
+    /// <returns>Timespan or null.</returns>
     public static TimeSpan? ToTimeSpan(this EstimatedTimeRequest? request) =>
         request is not null
             ? new TimeSpan((int)request.Days, (int)request.Hours, (int)request.Minutes, 0)
--- src/core/Codend.Contracts/Requests/Story/UpdateStoryRequest.cs
+++ src/core/Codend.Contracts/Requests/Story/UpdateStoryRequest.cs
@@ -5,6 +5,7 @@
 /// </summary>
 /// <param name="Name">Name of the story.</param>
 /// <param name="Description">Description of the story.</param>
+/// <param name="EpicId">EpicId of the story.</param>
 public record UpdateStoryRequest(
     string Name,
     string Description,
