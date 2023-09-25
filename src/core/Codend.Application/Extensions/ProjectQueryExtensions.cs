fef0b17bfb7de600b77c63bfcf66c3887b52b8c4
pzajaczkowski <demoon.pps@gmail.com>
2023-09-25 12:54:36 +0200

    Add extensions to IQueryable for Project and ProjectMember

---
+++ src/core/Codend.Application/Extensions/ProjectQueryExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Codend.Domain.Entities;
+
+namespace Codend.Application.Extensions;
+
+internal static class ProjectQueryExtensions
+{
+    internal static IQueryable<Project> GetProjectsWithIds(this IQueryable<Project> query, IEnumerable<ProjectId> projectIds)
+    {
+        return query.Where(x => projectIds.Contains(x.Id));
+    }
+}
