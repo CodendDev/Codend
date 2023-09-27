f5668eb5685bd6bb47fbca2e7f8b3f2dadecb941
pzajaczkowski <demoon.pps@gmail.com>
2023-09-27 16:13:06 +0200

    Add max project members const, validation and error.

--- src/core/Codend.Domain/Core/Errors/ProjectErrors.cs
+++ src/core/Codend.Domain/Core/Errors/ProjectErrors.cs
@@ -2,12 +2,21 @@
 
 public static partial class DomainErrors
 {
-    public class Project
+    public static class Project
     {
         public class ProjectHasToHaveProjectTaskStatus : DomainError
         {
             public ProjectHasToHaveProjectTaskStatus()
                 : base("Project.ProjectHasToHaveProjectTaskStatus", "Project has to have at least one task status")
+            {
+            }
+        }
+
+        public class ProjectHasMaximumNumberOfMembers : DomainError
+        {
+            public ProjectHasMaximumNumberOfMembers()
+                : base("Project.ProjectHasMaximumNumberOfMembers",
+                    $"Project already has the maximum number of members equal {Entities.Project.MaxMembersCount}.")
             {
             }
         }
