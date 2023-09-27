4b69f3f8f5e237b3635af4b06f2e34e2b3561e4d
pzajaczkowski <demoon.pps@gmail.com>
2023-09-27 14:29:53 +0200

    Add GetProjectTaskStatusesQuery and validator.

---
+++ src/core/Codend.Application/ProjectTaskStatuses/Queries/GetProjectTaskStatuses/GetProjectTaskStatusesQuery.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Codend.Application.Core.Abstractions.Authentication;
+using Codend.Application.Core.Abstractions.Data;
+using Codend.Application.Core.Abstractions.Messaging.Queries;
+using Codend.Application.Extensions;
+using Codend.Contracts.Responses.ProjectTaskStatus;
+using Codend.Domain.Entities;
+using FluentResults;
+using Microsoft.EntityFrameworkCore;
+using static Codend.Domain.Core.Errors.DomainErrors.General;
+
+namespace Codend.Application.ProjectTaskStatuses.Queries.GetProjectTaskStatuses;
+
+/// <summary>
+/// Query used for retrieving all project task statuses.
+/// </summary>
+/// <param name="ProjectId">Id of the project which statuses will be returned.</param>
+public sealed record GetProjectTaskStatusesQuery(ProjectId ProjectId) : IQuery<List<ProjectTaskStatusResponse>>;
+
+/// <summary>
+/// <see cref="GetProjectTaskStatusesQueryHandler"/> handler.
+/// </summary>
+public class
+    GetProjectTaskStatusesQueryHandler : IQueryHandler<GetProjectTaskStatusesQuery, List<ProjectTaskStatusResponse>>
+{
+    private readonly IQueryableSets _context;
+    private readonly IUserIdentityProvider _identityProvider;
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetProjectTaskStatusesQueryHandler"/> class.
+    /// </summary>
+    public GetProjectTaskStatusesQueryHandler(IQueryableSets context, IUserIdentityProvider identityProvider,
+        IMapper mapper)
+    {
+        _context = context;
+        _identityProvider = identityProvider;
+        _mapper = mapper;
+    }
+
+    /// <inheritdoc />
+    public async Task<Result<List<ProjectTaskStatusResponse>>> Handle(GetProjectTaskStatusesQuery request,
+        CancellationToken cancellationToken)
+    {
+        var userId = _identityProvider.UserId;
+        if (await _context.Queryable<ProjectMember>().IsUserMember(userId, request.ProjectId) is false)
+        {
+            return DomainNotFound.Fail<Project>();
+        }
+
+        var statuses = await _context.Queryable<ProjectTaskStatus>()
+            .Where(status => status.ProjectId == request.ProjectId)
+            .ProjectTo<ProjectTaskStatusResponse>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return Result.Ok(statuses);
+    }
+}
