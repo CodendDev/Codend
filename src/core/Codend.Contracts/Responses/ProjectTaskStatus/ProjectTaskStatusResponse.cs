c97b8567845cede9a97da21efb2678aff7f23b80
pzajaczkowski <demoon.pps@gmail.com>
2023-09-29 16:39:24 +0200

    Add id to status response and fix comments.

--- src/core/Codend.Contracts/Responses/ProjectTaskStatus/ProjectTaskStatusResponse.cs
+++ src/core/Codend.Contracts/Responses/ProjectTaskStatus/ProjectTaskStatusResponse.cs
@@ -3,8 +3,10 @@
 /// <summary>
 /// Represents default project task status response.
 /// </summary>
+/// <param name="Id">Id of the status.</param>
 /// <param name="Name">Name of the status.</param>
 public sealed record ProjectTaskStatusResponse
 (
+    Guid Id,
     string Name
 );
