1f67c9fc0430e5a89c77c79d8e88def8206435cc
pzajaczkowski <demoon.pps@gmail.com>
2023-10-05 14:24:08 +0200

    Add epic command handlers tests.

---
+++ tests/Codend.UnitTests/Application/CommandHandlers/Epics/CreateEpicCommandHandlerTests.cs
@@ -0,0 +1,110 @@
+﻿using Bogus;
+using Codend.Application.Core.Abstractions.Data;
+using Codend.Application.Epics.Commands.CreateEpic;
+using Codend.Domain.Core.Errors;
+using Codend.Domain.Core.Primitives;
+using Codend.Domain.Entities;
+using Codend.Domain.Repositories;
+using FluentAssertions;
+using Moq;
+
+namespace Codend.UnitTests.Application.CommandHandlers.Epics;
+
+public class CreateEpicCommandHandlerTests
+{
+    private readonly Mock<IEpicRepository> _epicRepository = new();
+    private readonly Mock<IUnitOfWork> _unitOfWork = new();
+    private readonly Mock<IProjectRepository> _projectRepository = new();
+    private readonly Mock<IProjectTaskStatusRepository> _statusRepository = new();
+    private readonly CreateEpicCommandHandler _instance;
+
+    public CreateEpicCommandHandlerTests()
+    {
+        _instance = new CreateEpicCommandHandler(
+            _epicRepository.Object,
+            _unitOfWork.Object,
+            _projectRepository.Object,
+            _statusRepository.Object);
+    }
+
+    [Fact]
+    public async Task Handle_WhenValidCommandPassed_ShouldPersistInEpicRepository()
+    {
+        // Arrange
+        var faker = new Faker();
+        var command = new CreateEpicCommand(
+            faker.Random.Word(),
+            faker.Lorem.Paragraph(),
+            faker.Random.Guid().GuidConversion<ProjectId>(),
+            faker.Random.Guid().GuidConversion<ProjectTaskStatusId>()
+        );
+        var project = Project.Create(faker.Random.Guid().GuidConversion<UserId>(), faker.Random.Word()).Value;
+        _projectRepository
+            .Setup(r => r.GetByIdAsync(command.ProjectId)).ReturnsAsync(project);
+        _statusRepository
+            .Setup(r => r.StatusExistsWithStatusIdAsync(command.StatusId!, command.ProjectId, CancellationToken.None))
+            .ReturnsAsync(true);
+
+        // Act
+        var result = await _instance.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        _epicRepository.Verify(r => r.Add(It.IsAny<Epic>()), Times.Once);
+        _unitOfWork.Verify(u => u.SaveChangesAsync(CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_WhenValidCommandPassedWithoutStatus_ShouldPersistInEpicRepositoryWithDefaultStatus()
+    {
+        // Arrange
+        var faker = new Faker();
+        var command = new CreateEpicCommand(
+            faker.Random.Word(),
+            faker.Lorem.Paragraph(),
+            faker.Random.Guid().GuidConversion<ProjectId>(),
+            null
+        );
+        var project = Project.Create(faker.Random.Guid().GuidConversion<UserId>(), faker.Random.Word()).Value;
+        project.EditDefaultStatus(faker.Random.Guid().GuidConversion<ProjectTaskStatusId>());
+        _projectRepository
+            .Setup(r => r.GetByIdAsync(command.ProjectId)).ReturnsAsync(project);
+
+        // Act
+        var result = await _instance.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        _epicRepository.Verify(r => r.Add(It.Is<Epic>(e => e.StatusId == project.DefaultStatusId)), Times.Once);
+        _unitOfWork.Verify(u => u.SaveChangesAsync(CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_WhenCommandWithInvalidStatusPassed_ShouldReturnFailedResultWithInvalidStatusIdError()
+    {
+        // Arrange
+        var faker = new Faker();
+        var command = new CreateEpicCommand(
+            faker.Random.Word(),
+            faker.Lorem.Paragraph(),
+            faker.Random.Guid().GuidConversion<ProjectId>(),
+            faker.Random.Guid().GuidConversion<ProjectTaskStatusId>()
+        );
+        var project = Project.Create(faker.Random.Guid().GuidConversion<UserId>(), faker.Random.Word()).Value;
+        project.EditDefaultStatus(faker.Random.Guid().GuidConversion<ProjectTaskStatusId>());
+        _projectRepository
+            .Setup(r => r.GetByIdAsync(command.ProjectId)).ReturnsAsync(project);
+        _statusRepository
+            .Setup(r => r.StatusExistsWithStatusIdAsync(command.StatusId!, command.ProjectId, CancellationToken.None))
+            .ReturnsAsync(false);
+
+        // Act
+        var result = await _instance.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.HasError<DomainErrors.ProjectTaskStatus.InvalidStatusId>().Should().BeTrue();
+        _epicRepository.Verify(r => r.Add(It.Is<Epic>(e => e.StatusId == project.DefaultStatusId)), Times.Never);
+        _unitOfWork.Verify(u => u.SaveChangesAsync(CancellationToken.None), Times.Never);
+    }
+}
---
+++ tests/Codend.UnitTests/Application/CommandHandlers/Epics/DeleteEpicCommandHandlerTests.cs
@@ -0,0 +1,80 @@
+﻿using Bogus;
+using Codend.Application.Core.Abstractions.Data;
+using Codend.Application.Epics.Commands.DeleteEpic;
+using Codend.Domain.Core.Errors;
+using Codend.Domain.Core.Primitives;
+using Codend.Domain.Entities;
+using Codend.Domain.Repositories;
+using FluentAssertions;
+using Moq;
+
+namespace Codend.UnitTests.Application.CommandHandlers.Epics;
+
+public class DeleteEpicCommandHandlerTests
+{
+    private readonly Mock<IEpicRepository> _epicRepository = new();
+    private readonly Mock<IStoryRepository> _storyRepository = new();
+    private readonly Mock<IUnitOfWork> _unitOfWork = new();
+    private readonly DeleteEpicCommandHandler _instance;
+
+    public DeleteEpicCommandHandlerTests()
+    {
+        _instance = new DeleteEpicCommandHandler(
+            _epicRepository.Object,
+            _storyRepository.Object,
+            _unitOfWork.Object);
+    }
+
+    [Fact]
+    public async Task
+        Handle_WhenValidCommandPassed_ShouldRemoveEpicInRepositoryAndReplaceDependentEpicIdInStoriesWithNull()
+    {
+        // Arrange
+        var faker = new Faker();
+        var epic = Epic.Create(
+            faker.Random.Word(),
+            faker.Lorem.Paragraph(),
+            faker.Random.Guid().GuidConversion<ProjectId>(),
+            faker.Random.Guid().GuidConversion<ProjectTaskStatusId>()).Value;
+        var command = new DeleteEpicCommand(
+            epic.Id
+        );
+        var storyList = new List<Story>
+        {
+            Story.Create(faker.Random.Word(), faker.Lorem.Paragraph(), epic.ProjectId, epic.Id, epic.StatusId).Value
+        };
+
+        _epicRepository.Setup(r => r.GetByIdAsync(command.EpicId, CancellationToken.None)).ReturnsAsync(epic);
+        _storyRepository.Setup(r => r.GetStoriesByEpicIdAsync(epic.Id, CancellationToken.None)).ReturnsAsync(storyList);
+
+        // Act
+        var result = await _instance.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        storyList.Should().OnlyContain(s => s.EpicId == null);
+        _storyRepository.Verify(r => r.UpdateRange(It.IsAny<IEnumerable<Story>>()), Times.Once);
+        _epicRepository.Verify(r => r.Remove(It.IsAny<Epic>()), Times.Once);
+        _unitOfWork.Verify(u => u.SaveChangesAsync(CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_WhenCommandWithInvalidIdPassed_ShouldReturnResultWithDomainNotFoundError()
+    {
+        // Arrange
+        var faker = new Faker();
+        var command = new DeleteEpicCommand(
+            faker.Random.Guid().GuidConversion<EpicId>()
+        );
+        _epicRepository.Setup(r => r.GetByIdAsync(command.EpicId, CancellationToken.None)).ReturnsAsync((Epic?)null);
+
+        // Act
+        var result = await _instance.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        _storyRepository.Verify(r => r.UpdateRange(It.IsAny<IEnumerable<Story>>()), Times.Never);
+        _epicRepository.Verify(r => r.Remove(It.IsAny<Epic>()), Times.Never);
+        _unitOfWork.Verify(u => u.SaveChangesAsync(CancellationToken.None), Times.Never);
+    }
+}
