dfd89d3d1d5a32365c996967074c7b83dcc1e027
pzajaczkowski <demoon.pps@gmail.com>
2023-10-08 20:15:46 +0200

    Create resolver class.

---
+++ src/core/Codend.Presentation/Infrastructure/Resolver.cs
@@ -0,0 +1,17 @@
+﻿namespace Codend.Presentation.Infrastructure;
+
+/// <summary>
+/// Class for resolving query/commands. Can authorize and return appriopriate ActionResult based on request result.
+/// </summary>
+/// <typeparam name="TRequest">Command/Query to be resolved.</typeparam>
+internal sealed class Resolver<TRequest>
+{
+    private Resolver(TRequest request)
+    {
+        Request = request;
+    }
+
+    public TRequest Request { get; private set; }
+
+    public static Resolver<TRequest> For(TRequest request) => new(request);
+}
