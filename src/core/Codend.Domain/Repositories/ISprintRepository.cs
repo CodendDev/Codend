face9a63b1a735047bb4717132c5c24e2fe12918
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-24 10:29:20 +0200

    Sprint repository update methods

--- src/core/Codend.Domain/Repositories/ISprintRepository.cs
+++ src/core/Codend.Domain/Repositories/ISprintRepository.cs
@@ -4,5 +4,7 @@ namespace Codend.Domain.Repositories;
 
 public interface ISprintRepository
 {
+    Task<Sprint?> GetByIdAsync(SprintId id);
+    void Update(Sprint sprint);
     void Add(Sprint sprint);
 }
