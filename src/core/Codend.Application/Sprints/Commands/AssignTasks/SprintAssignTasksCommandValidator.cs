ef68eab2892180aea83c1e3957591a5a0c6b8fb0
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-26 22:00:47 +0200

    Sprint assign task validator

---
+++ src/core/Codend.Application/Sprints/Commands/AssignTasks/SprintAssignTasksCommandValidator.cs
@@ -0,0 +1,29 @@
+using Codend.Application.Extensions;
+using FluentValidation;
+using static Codend.Application.Core.Errors.ValidationErrors.Common;
+
+namespace Codend.Application.Sprints.Commands.AssignTasks;
+
+/// <summary>
+/// Validates <see cref="SprintAssignTasksCommand"/>.
+/// </summary>
+public class SprintAssignTasksCommandValidator : AbstractValidator<SprintAssignTasksCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SprintAssignTasksCommandValidator"/> class.
+    /// </summary>
+    public SprintAssignTasksCommandValidator()
+    {
+        RuleFor(x => x.SprintId)
+            .NotEmpty()
+            .WithError(new PropertyNullOrEmpty(nameof(SprintAssignTasksCommand.SprintId)));
+
+        RuleFor(x => x.TasksIds)
+            .NotEmpty()
+            .WithError(new PropertyNullOrEmpty(nameof(SprintAssignTasksCommand.TasksIds)));
+
+        RuleForEach(x => x.TasksIds)
+            .NotEmpty()
+            .WithError(new PropertyNullOrEmpty("Task id with index of {CollectionIndex} is required"));
+    }
+}
