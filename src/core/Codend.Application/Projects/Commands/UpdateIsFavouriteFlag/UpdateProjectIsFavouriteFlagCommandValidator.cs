a2b3105fb901327e1007102401a0a244c2e32cc0
pzajaczkowski <demoon.pps@gmail.com>
2023-10-27 11:00:47 +0200

    Add validator for update isFavourite.

---
+++ src/core/Codend.Application/Projects/Commands/UpdateIsFavouriteFlag/UpdateProjectIsFavouriteFlagCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Codend.Application.Extensions;
+using FluentValidation;
+using static Codend.Application.Core.Errors.ValidationErrors.Common;
+
+namespace Codend.Application.Projects.Commands.UpdateIsFavouriteFlag;
+
+/// <summary>
+/// Validates <see cref="UpdateProjectIsFavouriteFlagCommand"/>.
+/// </summary>
+public class UpdateProjectIsFavouriteFlagCommandValidator : AbstractValidator<UpdateProjectIsFavouriteFlagCommand>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UpdateProjectIsFavouriteFlagCommandValidator"/> class.
+    /// </summary>
+    public UpdateProjectIsFavouriteFlagCommandValidator()
+    {
+        RuleFor(x => x.ProjectId)
+            .NotEmpty()
+            .WithError(new PropertyNullOrEmpty(nameof(UpdateProjectIsFavouriteFlagCommand.ProjectId)));
+
+        RuleFor(x => x.IsFavourite)
+            .NotNull()
+            .WithError(new PropertyNullOrEmpty(nameof(UpdateProjectIsFavouriteFlagCommand.IsFavourite)));
+    }
+}
