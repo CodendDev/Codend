1de80ce6fee9826ca3c73005651f01957e0021c8
pzajaczkowski <demoon.pps@gmail.com>
2023-10-27 10:59:31 +0200

    Modify GetProjectsQuery.cs to fetch isFavourite.

--- src/core/Codend.Application/Projects/Queries/GetProjects/GetProjectsQuery.cs
+++ src/core/Codend.Application/Projects/Queries/GetProjects/GetProjectsQuery.cs
@@ -1,5 +1,3 @@
-using AutoMapper;
-using AutoMapper.QueryableExtensions;
 using Codend.Application.Core.Abstractions.Authentication;
 using Codend.Application.Core.Abstractions.Data;
 using Codend.Application.Core.Abstractions.Messaging.Queries;
@@ -37,7 +35,6 @@ public sealed record GetProjectsQuery(
 /// </summary>
 public class GetProjectsQueryHandler : IQueryHandler<GetProjectsQuery, PagedList<ProjectResponse>>
 {
-    private readonly IMapper _mapper;
     private readonly IHttpContextProvider _contextProvider;
     private readonly IQueryableSets _queryableSets;
 
@@ -45,11 +42,9 @@ public class GetProjectsQueryHandler : IQueryHandler<GetProjectsQuery, PagedList
     /// Initializes a new instance of the <see cref="GetProjectsQueryHandler"/> class.
     /// </summary>
     public GetProjectsQueryHandler(
-        IMapper mapper,
         IHttpContextProvider contextProvider,
         IQueryableSets queryableSets)
     {
-        _mapper = mapper;
         _contextProvider = contextProvider;
         _queryableSets = queryableSets;
     }
@@ -59,19 +54,39 @@ public class GetProjectsQueryHandler : IQueryHandler<GetProjectsQuery, PagedList
         CancellationToken cancellationToken)
     {
         var userId = _contextProvider.UserId;
-        var userProjects = _queryableSets.Queryable<ProjectMember>().GetUserProjectsIds(userId);
-        var projectsQuery = _queryableSets.Queryable<Project>().GetProjectsWithIds(userProjects);
+        var projectsQuery = _queryableSets.Queryable<Project>();
 
-        var projects = await projectsQuery
-            .Search(query.Search)
-            .Sort(query, ProjectSortColumnSelector.SortColumnSelector(query.SortColumn))
+        var projectsResponseQuery = _queryableSets.Queryable<ProjectMember>()
+            .GetForUser(userId)
+            .Join(
+                projectsQuery,
+                projectMember => projectMember.ProjectId,
+                project => project.Id,
+                (projectMember, project) => new
+                {
+                    project.Id,
+                    project.Name,
+                    project.Description,
+                    project.OwnerId,
+                    projectMember.IsFavourite
+                }
+            );
+        var searchedProjects = query.Search is null
+            ? projectsResponseQuery
+            : projectsResponseQuery
+                .Where(project => project.Name.Value.ToLower().Contains(query.Search.ToLower()));
+
+        var projects = await searchedProjects
+            .OrderByDescending(project => project.IsFavourite)
             .Paginate(query)
-            .ProjectTo<ProjectResponse>(_mapper.ConfigurationProvider)
             .ToListAsync(cancellationToken);
 
-        var totalCount = await projectsQuery.CountAsync(cancellationToken);
+        var projectsResponse = projects.Select(x =>
+            new ProjectResponse(x.Id.Value, x.Name.Value, x.Description.Value, x.OwnerId.Value, x.IsFavourite));
 
-        var result = new PagedList<ProjectResponse>(projects, query.PageIndex, query.PageSize, totalCount);
+        var totalCount = await projectsResponseQuery.CountAsync(cancellationToken);
+
+        var result = new PagedList<ProjectResponse>(projectsResponse, query.PageIndex, query.PageSize, totalCount);
         return Result.Ok(result);
     }
 }
