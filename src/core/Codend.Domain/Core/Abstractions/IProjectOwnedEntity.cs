a25d3d2a250caae04b18b4d5c1533e55f6f203e2
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-30 16:26:22 +0100

    IProjectOwnedEntity documentation 💀

--- src/core/Codend.Domain/Core/Abstractions/IProjectOwnedEntity.cs
+++ src/core/Codend.Domain/Core/Abstractions/IProjectOwnedEntity.cs
@@ -2,8 +2,5 @@ namespace Codend.Domain.Entities;
 
 public interface IProjectOwnedEntity
 {
-    /// <summary>
-    /// ProjectId which story belongs to.
-    /// </summary>
     ProjectId ProjectId { get; }
 }
