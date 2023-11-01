5e88f7cfd737b5a2dfa30f40017f0c891d049943
pzajaczkowski <demoon.pps@gmail.com>
2023-11-01 12:57:17 +0100

    Add Position to ProjectTaskStatus entity.

--- src/core/Codend.Domain/Entities/ProjectTaskStatus/ProjectTaskStatus.cs
+++ src/core/Codend.Domain/Entities/ProjectTaskStatus/ProjectTaskStatus.cs
@@ -1,6 +1,7 @@
 using Codend.Domain.Core.Extensions;
 using Codend.Domain.Core.Primitives;
 using Codend.Domain.ValueObjects;
+using Codend.Shared.Infrastructure.Lexorank;
 using FluentResults;
 
 namespace Codend.Domain.Entities;
@@ -15,14 +16,16 @@ public class ProjectTaskStatus : Entity<ProjectTaskStatusId>
 
     public ProjectTaskStatusName Name { get; private set; }
     public ProjectId ProjectId { get; private set; }
+    public Lexorank? Position { get; private set; }
 
     /// <summary>
     /// Creates new projectTaskStatus.
     /// </summary>
     /// <param name="projectId">Id of project it belongs to.</param>
     /// <param name="name">Name for the status.</param>
+    /// <param name="position">Status position.</param>
     /// <returns>Ok result with newly created ProjectTaskStatus object or an error.</returns>
-    public static Result<ProjectTaskStatus> Create(ProjectId projectId, string name)
+    public static Result<ProjectTaskStatus> Create(ProjectId projectId, string name, Lexorank? position)
     {
         var status = new ProjectTaskStatus()
         {
@@ -31,6 +34,7 @@ public class ProjectTaskStatus : Entity<ProjectTaskStatusId>
 
         var nameResult = ProjectTaskStatusName.Create(name);
         status.Name = nameResult.ValueOrDefault;
+        status.Position = position;
 
         return Result.Ok(status).MergeReasons(nameResult.ToResult());
     }
@@ -46,5 +50,16 @@ public class ProjectTaskStatus : Entity<ProjectTaskStatusId>
         this.Name = nameResult.ValueOrDefault;
 
         return Result.Ok(this).MergeReasons(nameResult.ToResult());
+    }
+
+    /// <summary>
+    /// Edits status position.
+    /// </summary>
+    /// <param name="position">Lexorank position.</param>
+    /// <returns>Ok result with edited <see cref="ProjectTaskStatus"/>.</returns>
+    public Result<ProjectTaskStatus> EditPosition(Lexorank? position)
+    {
+        Position = position;
+        return Result.Ok(this);
     }
 }
