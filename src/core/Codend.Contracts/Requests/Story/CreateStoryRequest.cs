156adf8cb98233c5cd66267d9686d4edd537c52b
pzajaczkowski <demoon.pps@gmail.com>
2023-11-15 16:22:09 +0100

    Add sprintId to create story request and assign story to sprint after creation.

--- src/core/Codend.Contracts/Requests/Story/CreateStoryRequest.cs
+++ src/core/Codend.Contracts/Requests/Story/CreateStoryRequest.cs
@@ -7,9 +7,11 @@
 /// <param name="Description">Description of the story.</param>
 /// <param name="EpicId">EpicId of the story.</param>
 /// <param name="StatusId">Story status.</param>
+/// <param name="SprintId">Id of the sprint to which epic will be assigned.</param>;
 public record CreateStoryRequest(
     string Name,
     string Description,
     Guid? EpicId,
-    Guid? StatusId
+    Guid? StatusId,
+    Guid? SprintId
 );
