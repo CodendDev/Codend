35f1b7a0fcfede1bd6bc58f2a815730b3e8ec2ce
pzajaczkowski <demoon.pps@gmail.com>
2023-11-15 16:21:33 +0100

    Add sprintId to create task request, and assign task to sprint after creation.

--- src/core/Codend.Application/ProjectTasks/Commands/CreateProjectTask/CreateBaseProjectTaskCommand.cs
+++ src/core/Codend.Application/ProjectTasks/Commands/CreateProjectTask/CreateBaseProjectTaskCommand.cs
@@ -4,6 +4,7 @@ using Codend.Application.Core.Abstractions.Messaging.Commands;
 using Codend.Application.ProjectTasks.Commands.CreateProjectTask.Abstractions;
 using Codend.Domain.Entities;
 using Codend.Domain.Repositories;
+using MediatR;
 
 namespace Codend.Application.ProjectTasks.Commands.CreateProjectTask;
 
@@ -11,9 +12,11 @@ namespace Codend.Application.ProjectTasks.Commands.CreateProjectTask;
 /// Command for creating <see cref="BaseProjectTask"/>.
 /// </summary>
 /// <param name="TaskProperties"><see cref="BaseProjectTaskCreateProperties"/> properties.</param>
+/// <param name="SprintId">Id of the sprint to which epic will be assigned.</param>
 public sealed record CreateBaseProjectTaskCommand
 (
-    BaseProjectTaskCreateProperties TaskProperties
+    BaseProjectTaskCreateProperties TaskProperties,
+    SprintId? SprintId
 ) : ICommand<Guid>, ICreateProjectTaskCommand<BaseProjectTaskCreateProperties>;
 
 /// <summary>
@@ -34,13 +37,15 @@ public class CreateBaseProjectTaskCommandHandler :
         IHttpContextProvider contextProvider,
         IProjectMemberRepository projectMemberRepository,
         IStoryRepository storyRepository,
-        IProjectTaskStatusRepository statusRepository)
+        IProjectTaskStatusRepository statusRepository,
+        IMediator mediator)
         : base(projectTaskRepository,
             unitOfWork,
             contextProvider,
             projectMemberRepository,
             storyRepository,
-            statusRepository)
+            statusRepository,
+            mediator)
     {
     }
 }
