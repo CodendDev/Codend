dd591cf3edd227437ed14376c70e4508d9b251c2
pzajaczkowski <demoon.pps@gmail.com>
2023-11-29 21:10:10 +0100

    Add Required Props to MoveSprintTaskCommand.cs and edit status in task when provided.

--- src/core/Codend.Application/Sprints/Commands/MoveTask/MoveSprintTaskCommand.cs
+++ src/core/Codend.Application/Sprints/Commands/MoveTask/MoveSprintTaskCommand.cs
@@ -2,6 +2,7 @@ using Codend.Application.Core.Abstractions.Data;
 using Codend.Application.Core.Abstractions.Messaging.Commands;
 using Codend.Domain.Core.Abstractions;
 using Codend.Domain.Core.Errors;
+using Codend.Domain.Core.Primitives;
 using Codend.Domain.Entities;
 using Codend.Domain.Repositories;
 using Codend.Shared.Infrastructure.Lexorank;
@@ -13,11 +14,22 @@ namespace Codend.Application.Sprints.Commands.MoveTask;
 /// <summary>
 /// Command used for moving sprint task to other position.
 /// </summary>
+/// <param name="ProjectId">Id of the project task belongs to.</param>
 /// <param name="SprintId">Id of the sprint task belongs to.</param>
 /// <param name="TaskId">If of the task that will be moved.</param>
 /// <param name="Prev">Lexorank position after which tasks will be inserted.</param>
 /// <param name="Next">Lexorank position before which tasks will be inserted.</param>
-public record MoveSprintTaskCommand(SprintId SprintId, ISprintTaskId TaskId, string? Prev, string? Next) : ICommand;
+/// <param name="StatusId">New statusId for the task.</param>
+/// <param name="Type">Task type.</param>
+public record MoveSprintTaskCommand(
+    ProjectId ProjectId,
+    SprintId SprintId,
+    ISprintTaskId TaskId,
+    string? Prev,
+    string? Next,
+    ProjectTaskStatusId? StatusId,
+    string Type
+) : ICommand;
 
 /// <summary>
 /// <see cref="MoveSprintTaskCommand"/> handler.
@@ -26,15 +38,28 @@ public class MoveSprintTaskCommandHandler : ICommandHandler<MoveSprintTaskComman
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly ISprintProjectTaskRepository _sprintProjectTaskRepository;
+    private readonly IProjectTaskRepository _projectTaskRepository;
+    private readonly IStoryRepository _storyRepository;
+    private readonly IEpicRepository _epicRepository;
+    private readonly IProjectTaskStatusRepository _statusRepository;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="MoveSprintTaskCommandHandler"/> class.
     /// </summary>
-    public MoveSprintTaskCommandHandler(IUnitOfWork unitOfWork,
-        ISprintProjectTaskRepository sprintProjectTaskRepository)
+    public MoveSprintTaskCommandHandler(
+        IUnitOfWork unitOfWork,
+        ISprintProjectTaskRepository sprintProjectTaskRepository,
+        IProjectTaskRepository projectTaskRepository,
+        IStoryRepository storyRepository,
+        IEpicRepository epicRepository,
+        IProjectTaskStatusRepository statusRepository)
     {
         _unitOfWork = unitOfWork;
         _sprintProjectTaskRepository = sprintProjectTaskRepository;
+        _projectTaskRepository = projectTaskRepository;
+        _storyRepository = storyRepository;
+        _epicRepository = epicRepository;
+        _statusRepository = statusRepository;
     }
 
     /// <inheritdoc />
@@ -52,6 +77,22 @@ public class MoveSprintTaskCommandHandler : ICommandHandler<MoveSprintTaskComman
             return Result.Fail(new DomainErrors.Sprint.TaskIsNotAssignedToSprint());
         }
 
+        if (request.StatusId is not null)
+        {
+            var statusExist = await _statusRepository.StatusExistsWithStatusIdAsync(request.StatusId, request.ProjectId,
+                cancellationToken);
+            if (!statusExist)
+            {
+                return DomainNotFound.Fail<ProjectTaskStatus>();
+            }
+
+            var result = await EditSprintTaskStatus(request, cancellationToken);
+            if (result.IsFailed)
+            {
+                return result;
+            }
+        }
+
         var prev = request.Prev is not null ? new Lexorank(request.Prev) : null;
         var next = request.Next is not null ? new Lexorank(request.Next) : null;
         var midPosition = Lexorank.GetMiddle(prev, next);
@@ -61,5 +102,49 @@ public class MoveSprintTaskCommandHandler : ICommandHandler<MoveSprintTaskComman
         await _unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Result.Ok();
+    }
+
+    private async Task<Result> EditSprintTaskStatus(MoveSprintTaskCommand request, CancellationToken cancellationToken)
+    {
+        switch (request.Type.ToLower())
+        {
+            case "task":
+            case "base":
+            case "bugfix":
+                var task = await _projectTaskRepository.GetByIdAsync(
+                    request.TaskId.Value.GuidConversion<ProjectTaskId>(), cancellationToken);
+                if (task is null)
+                {
+                    return DomainNotFound.Fail<BaseProjectTask>();
+                }
+
+                task.EditStatus(request.StatusId!);
+                _projectTaskRepository.Update(task);
+                return Result.Ok();
+            case "story":
+                var story = await _storyRepository.GetByIdAsync(request.TaskId.Value.GuidConversion<StoryId>(),
+                    cancellationToken);
+                if (story is null)
+                {
+                    return DomainNotFound.Fail<Story>();
+                }
+
+                story.EditStatus(request.StatusId!);
+                _storyRepository.Update(story);
+                return Result.Ok();
+            case "epic":
+                var epic = await _epicRepository.GetByIdAsync(request.TaskId.Value.GuidConversion<EpicId>(),
+                    cancellationToken);
+                if (epic is null)
+                {
+                    return DomainNotFound.Fail<Epic>();
+                }
+
+                epic.EditStatus(request.StatusId!);
+                _epicRepository.Update(epic);
+                return Result.Ok();
+            default:
+                throw new ArgumentException("Unsupported task type");
+        }
     }
 }
