0318b10860df8c8af093dcb4ab3f76fca97e1ad9
pzajaczkowski <demoon.pps@gmail.com>
2024-01-02 15:56:11 +0100

    Fix email notification messages.

--- src/core/notifications/Codend.Notifications.Email/Notifications/UserAddedToProject.cs
+++ src/core/notifications/Codend.Notifications.Email/Notifications/UserAddedToProject.cs
@@ -1,4 +1,5 @@
 using Codend.Application.Core.Abstractions.Services;
+using Codend.Contracts.Responses;
 using Codend.Domain.Core.Events;
 using Codend.Notifications.Email.Abstractions;
 using Codend.Notifications.Email.Notifications.Core;
@@ -12,7 +13,9 @@ public class UserAddedToProject : UserEmailNotificationAbstractHandler<UserAdded
     {
     }
 
-    protected override string GetEmailSubject(UserAddedToProjectEvent notification) => "dodany";
+    protected override string GetEmailSubject(UserAddedToProjectEvent notification) =>
+        "[Codend] You have been assigned to new task!📝";
 
-    protected override string GetEmailMessage(UserAddedToProjectEvent notification) => "do projektu 💀";
+    protected override string GetEmailMessage(UserAddedToProjectEvent notification, UserDetails receiver) =>
+        $"Hello {receiver.FirstName},\n\nYou've been added to a new project. Please take a moment to review the project details and familiarize yourself with the ongoing tasks. \n\nBest regards, \n Codend";
 }
